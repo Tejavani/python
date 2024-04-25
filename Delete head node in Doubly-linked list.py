20d9759b13c77d1db81bc3b4000b580a38cbdf38
CH BHARGHAVATEJA VARDHAN <88646373+Tejavani@users.noreply.github.com>
2024-04-25 10:09:26 +0530

    Create Delete head node in Doubly-linked list.py
    Delete head node in Doubly-linked list

---
+++ Delete head node in Doubly-linked list.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+        self.tail = new_node
+
+    def delete_first(self):
+        if self.head is None:
+            return
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+
+    def display_forward(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+    def display_backward(self):
+        current = self.tail
+        while current:
+            print(current.data, end=" ")
+            current = current.prev
+        print()
+
+
+n = int(input())
+elements = list(map(int, input().split()))
+
+
+curr = DoublyLinkedList()
+for element in elements:
+    curr.insert_end(element)
+
+
+
+curr.display_forward()
+curr.display_backward()
+
+
+curr.delete_first()
+
+curr.display_forward()
+curr.display_backward()
